aa8b327f8fa0244c7c1b51c6247f15c7b5b7fe79
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-09 01:39:05 +0530

    map initialize at centre coordinates

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,8 @@ if (navigator.geolocation) {
     }
   );
 }
+
+const map = L.map("map").setView([0, 0], 10);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Het Patel",
+}).addTo(map);
